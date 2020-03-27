f3bf912a00e2900937d0a5dd4a284b0a5fb63084
chic-caller-7520 <arbrown110@gmail.com>
2020-03-27 00:37:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,11 @@
 def countdown
   while number <0
     puts "#{number} SECOND(S)!"
-    number-= 1 
+    number-= 1
+    if number < = 0 
+      break
  end 
   puts "HAPPY NEW YEAR!" 
 
 end
+def countdown_with_sleep
