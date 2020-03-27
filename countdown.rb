bb4019669b7ea794fd95335d3f759f19200d6494
chic-caller-7520 <arbrown110@gmail.com>
2020-03-27 00:29:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
-
-while number== 
-puts "#{number} SECOND(S)!"
- number-= 1 
- end 
-puts  
 def countdown
+  while number <0
+    puts "#{number} SECOND(S)!"
+    number-= 1 
+ end 
+  puts "HAPPY NEW YEAR!" 
+
 end
