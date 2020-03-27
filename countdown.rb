43af7165abfa665d447bb1bb0b931ad0266a9d18
chic-caller-7520 <arbrown110@gmail.com>
2020-03-27 01:10:17 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,11 +3,11 @@ def countdown(int)
   while (int > 0)
     puts "#{int} SECOND(S)!"
       int -= 1
-      break
+      
   end 
     "HAPPY NEW YEAR!" 
 end
-countdown (10)
+
 def countdown_with_sleep()
   S= 5
   while ""
